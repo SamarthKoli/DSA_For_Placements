baad80f7d5047d7c5972d00a2a9e094f80697713
samarthkoli <154528119+SamarthKoli@users.noreply.github.com>
2024-12-14 23:52:34 +0530

    Patterns Code

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -1,18 +1,20 @@
+package Patterns;
+
 import java.util.Scanner;
 
 public class Pattern1 {
     public static void main(String[] args) {
+
         Scanner sc=new Scanner(System.in);
+
         int row=sc.nextInt();
         int col=sc.nextInt();
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                System.out.print("*");
-                
+                System.err.print("* ");
             }
-            System.out.println("");
+            System.out.println(" ");
         }
-
     }
 }
