f07b227e65f21287e8a3029e3643e7453b522e63
samarthkoli <154528119+SamarthKoli@users.noreply.github.com>
2024-12-15 20:01:57 +0530

    Palindrome code added

---
+++ BasicMaths/IntegerPalindrome.java
@@ -0,0 +1,27 @@
+package BasicMaths;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        int temp=x;
+
+        int rev=0;
+
+        while(temp>0){
+            int d=temp%10;
+
+            if(rev==0 && d==0){
+                temp /= 10;
+                continue;
+            }
+            rev=rev*10+d;
+            temp=temp/10;
+        }
+        if(rev==x){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
