923568591c8aade294d8b95423664398585c6761
samarthkoli <154528119+SamarthKoli@users.noreply.github.com>
2024-12-20 13:46:37 +0530

    Selection sort added

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+class Solution {
+    void selectionSort(int[] arr) {
+       
+        
+        for(int i=0;i<=arr.length-1;i++){
+             int min=i;
+            for(int j=i;j<=arr.length-1;j++){
+                if (arr[j]<arr[min]){
+                   
+                   min=j;
+                }
+               
+                
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+            
+        }
+        // code here
+    }
+}
